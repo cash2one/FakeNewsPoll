008dbfcdf7391cc99e66a35fe681de5bbf4c3608
benjadahl <benjadahl@gmail.com>
2017-02-19 01:27:01 +0100

    Set up basic structure of application

---
+++ server.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+from flask import *
+import sqlite3
+import requests
+import json
+
+app = Flask(__name__)
+
+with open("config.json") as data_file:
+    config = json.load(data_file)
+
+def initDb():
+    db = sqlite3.connect("news.db")
+    db.execute("""CREATE TABLE IF NOT EXISTS news(
+    id INTEGER PRIMARY KEY NOT NULL,
+    title TEXT,
+    author TEXT,
+    description TEXT,
+    url TEXT,
+    urlToImage TEXT,
+    publishedAt TEXT,
+    voteTrue INTEGER,
+    voteFalse INTEGER
+    )""")
+
+def getNews():
+    url="https://newsapi.org/v1/articles?source=the-washington-post&sortBy=top&apiKey=" + config["API_KEY"]
+    response = requests.get(url)
+    newsData = json.loads(response.text)
+    return newsData
+
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    initDb()
+    getNews()
+    app.run(debug=True, host="0.0.0.0")
